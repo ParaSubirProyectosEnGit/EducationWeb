32b56ffbdb6393ab334c0d0cd203d0d843521bb6
ParaSubirProyectosEnGit <parasubirproyectosengit@gmail.com>
2022-06-02 11:56:12 +0200

    Version 5.01

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,9 +42,6 @@
                 <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
                     @guest
                         <a class="no-underline hover:underline" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        @if (Route::has('register'))
-                            <a class="no-underline hover:underline" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        @endif
                     @else
                         <span>{{ Auth::user()->name }}</span>
 
